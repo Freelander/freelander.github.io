e4976064a1df9a6b4d26ba3be1f527f564b833dc
Freelander <huanggaojun13@gmail.com>
2021-08-06 23:28:58 +0800

    New style blog

---
+++ js/zozo.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// back-to-top
+$(document).ready((function (_this) {
+  return function () {
+    let bt
+    bt = $('#back_to_top')
+    if ($(document).width() > 480) {
+      $(window).scroll(function () {
+        let st
+        st = $(window).scrollTop()
+        if (st > 30) {
+          return bt.css('display', 'block')
+        } else {
+          return bt.css('display', 'none')
+        }
+      })
+      return bt.click(function () {
+        $('body,html').animate({
+          scrollTop: 0,
+        }, 800)
+        return false
+      })
+    }
+  }
+})(this))
+
+// nav-toggle
+$(document).ready((function (_this) {
+  return function () {
+    let nav,icon
+    icon = $('#menu_icon')
+    nav = $('#site_nav')
+    icon.click(function () {
+      nav.slideToggle(250)
+    })
+  }
+})(this))
+
+// FancyBox
+$('[data-fancybox="gallery"]').fancybox({
+  arrows: false,
+  infobar: false,
+  buttons: [],
+  clickContent: "close",
+  autoFocus: false,
+  backFocus: false,
+  wheel: false,
+  mobile: {
+    clickContent: "close",
+    clickSlide: "close",
+    dblclickContent: false,
+    dblclickSlide: false
+  },
+});
